4ef97efde45cd80525e0840b0b37b0032851381d
Leandro Teixeira da Silva - LSI <leandro.silva@iteris.com.br>
2025-12-06 15:28:39 -0300

    up

---
+++ Python/ContentFabric/5youtube-upload/helper_AnalisePadraoJson.py
@@ -0,0 +1,10 @@
+import json, os
+
+path = r"C:\Users\leand\LTS - CONSULTORIA E DESENVOLVtIMENTO DE SISTEMAS\LTS SP Site - VideosGeradosPorScript\Videos\flavored_antacid.json"
+
+print("EXISTE? ", os.path.exists(path))
+
+with open(path, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+print("CHAVES DO JSON: ", list(data.keys()))
